a3154cad05a8d3d1595ecb870443a08dfac21fb3
amanda-vlopes <amandavitor@hotmail.com>
2023-04-19 18:22:06 -0300

    Requisito 2 finalizado

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Search extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" name="" id="" />
+        <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h3>
+      </div>
+    );
+  }
+}
