30dd20ef883dd03f7edcc3deafddb6fb25326e7b
rayan6ms <rayan6ms@gmail.com>
2023-04-20 19:56:33 -0300

    requisito 8

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import './ShoppingCart.css';
 
 export default class ShoppingCart extends Component {
   render() {
+    const savedProducts = JSON.parse(localStorage.getItem('cart') || []);
+    console.log(savedProducts.length);
     return (
       <div>
-        <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>
+        { !savedProducts.length ? (
+          <h3 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h3>)
+          : (
+            savedProducts.map(({ id, title, img, price, quantity }) => (
+              <div
+                key={ id }
+              >
+                <img src={ img } alt={ title } />
+                <p
+                  data-testid="shopping-cart-product-name"
+                >
+                  {title}
+                </p>
+                <p
+                  data-testid="shopping-cart-product-quantity"
+                >
+                  {quantity}
+                </p>
+                <p>{price}</p>
+              </div>
+            )))}
       </div>
     );
   }
