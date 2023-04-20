267b5eaddf927633fdcb17c94178137400749177
ferfachin <fernandofachinrm@gmail.com>
2023-04-20 10:39:53 -0400

    Estilizando Home e header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import ShoppingCart from './pages/ShoppingCart';
 import Home from './pages/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/shoppingcart" component={ ShoppingCart } />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import logo from '../assets/logo.svg';
+import './Header.css';
 
 export default class Header extends Component {
   render() {
     return (
-      <header>
-        <h1>FRONT-END Online Store</h1>
+      <header className="nav__header">
+        {/*  <Input /> */}
+        <img src={ logo } alt="Logo" />
+        <p>shopping Icon</p>
       </header>
     );
   }
